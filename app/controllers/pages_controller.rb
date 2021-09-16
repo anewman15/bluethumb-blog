7c0dfef574c87af14a6913fdcf47feffffed52bc
Abdullah Numan <anewman15@hotmail.com>
2021-09-17 01:50:19 +0600

    Handle api request error in Pages#home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def home
-    response = HTTParty.get('http://localhost:4000/posts')
-    @post = JSON.parse(response.body)['posts'].first if response.code == 200
+    @post = {}
+    begin
+      response = HTTParty.get('http://localhost:4000/posts')
+      @post = JSON.parse(response.body)['posts'].first if response && response.code == 200
+    rescue StandardError
+      render template: 'pages/server_error'
+    end
   end
 end
