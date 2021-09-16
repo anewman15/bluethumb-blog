263bb18ff1bc9c723783acf64a3984bb3df97b33
Abdullah Numan <anewman15@hotmail.com>
2021-09-17 05:05:35 +0600

    Add capybara to spec_helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,9 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'capybara/rspec'
+# require 'capybara/rails'
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -94,4 +97,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.include Capybara::DSL
 end
