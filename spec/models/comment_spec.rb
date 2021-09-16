29450bfff3547c1488815403d306a335b835eaa9
Abdullah Numan <anewman15@hotmail.com>
2021-09-17 04:15:04 +0600

    Write validation tests for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { FactoryBot.build :comment }
+
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :body }
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe 'validations' do
-    subject { FactoryBot.build(:post) }
+    subject { FactoryBot.build :post }
 
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:body) }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
   end
 end
