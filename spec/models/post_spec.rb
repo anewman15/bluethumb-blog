f03101ff8795a9675260675035241236f7513d72
Abdullah Numan <anewman15@hotmail.com>
2021-09-17 04:10:10 +0600

    Write validation tests for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { FactoryBot.build(:post) }
+
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
 end
