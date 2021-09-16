5ae9d5424983f9291f90928ab417129d7d0864b2
Abdullah Numan <anewman15@hotmail.com>
2021-09-17 01:01:48 +0600

    Clean up code, define Comments#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,58 +1,44 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  # GET /comments
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
-  # POST /comments
   def create
     @comment = Comment.new(comment_params)
 
-    if @comment.save
-      redirect_to @comment, notice: 'Comment was successfully created.'
+    if @comment.valid?
+      begin
+        response = HTTParty.post("http://localhost:4000/posts/#{params[:post_id]}/comments", comment_options)
+        if response && response.code == 201
+          @comment = JSON.parse(response.body)['comment']
+          redirect_to post_path(params[:post_id]), success: 'Comment added.'
+        end
+      rescue StandardError
+        redirect_to post_path(params[:post_id]), error: 'Something went wrong. Please try again.'
+      end
     else
-      render :new
+      redirect_to post_path(params[:post_id]), error: comment_error
     end
-  end
-
-  # PATCH/PUT /comments/1
-  def update
-    if @comment.update(comment_params)
-      redirect_to @comment, notice: 'Comment was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /comments/1
-  def destroy
-    @comment.destroy
-    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.fetch(:comment, {})
+  def comment_error
+    message = ''
+    @comment.errors.full_messages.each do |m|
+      message += "---#{m}\n"
     end
+    message
+  end
+
+  def comment_options
+    {
+      body: {
+        comment: {
+          name: comment_params[:name],
+          body: comment_params[:body]
+        }
+      }
+    }
+  end
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :name, :body)
+  end
 end
