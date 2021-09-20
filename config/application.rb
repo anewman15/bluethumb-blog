f8960bbfb369ca0c64dfc55b4992a04e92a09281
Abdullah Numan <anewman15@hotmail.com>
2021-09-20 14:39:46 +0600

    Add rack-cors gem, set up cors policy

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module BluethumbBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ['localhost:3001', 'https://anewman15-dev-point.netlify.app']
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
+      end
+    end
   end
 end
